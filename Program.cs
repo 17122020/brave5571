ef013c2207c78694c63148be58cd5fbfc6a0704e
Karyna Alexandrovich <karynaalex2020@mail.ru>
2024-01-05 14:22:13 +0300

    Задача на тему: два целых числа. Является ли первое число квадратом второго

--- Program.cs
+++ Program.cs
@@ -1,13 +1,5 @@
 ﻿// Комментарий однострочный
 
-// Напишите программу, которая на вход
-// принимает два целых числа и проверяет,
-// является ли первое число квадратом второго.
-// a = 25, b = 5 => да
-// a = 2, b = 10 => нет
-// a = 9, b = -3 => да
-// a = -3, b = 9 => нет
-
 /*
 Напишите программу, которая на вход
 принимает два целых числа и проверяет,
@@ -18,8 +10,18 @@ a = 9, b = -3 => да
 a = -3, b = 9 => нет
 */
 
-// тип_данных имя_переменной = значение;
-// camelCase: firstNumber, secondNumber
-// int number = 9; // number = 9
-// int result = number * number; // 81
+Console.Write("Введите первое число: ");
+int firstNumber = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите первое число: ");
+int secondNumber = Convert.ToInt32(Console.ReadLine());
+// f = 25; s = 5: f == s*s, 25 == 5 * 5
+if (firstNumber == secondNumber * secondNumber) 
+{
+    Console.WriteLine("Да, " + firstNumber + " является кв. от числа " + secondNumber);
+}
+else 
+{
+    Console.WriteLine("НЕТ, " + firstNumber + "  НЕ является кв. от числа " + secondNumber);
+}
+
 
